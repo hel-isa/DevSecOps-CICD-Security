230dfb143b7a6c95f8ef41a4724bd0613c365f2d
Hel.isa R <17197528+hel-isa@users.noreply.github.com>
2025-03-23 21:15:30 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
     return "Hello, DevSecOps!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
